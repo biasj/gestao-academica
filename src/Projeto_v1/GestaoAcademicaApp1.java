bd72d43783abe67adc82d667167eafbfad55ddb8
frostmarchesan <55881881+frostmarchesan@users.noreply.github.com>
2020-10-07 12:12:06 -0300

    segunda alteração - print screen - gestao academica

--- src/Projeto_v1/GestaoAcademicaApp1.java
+++ src/Projeto_v1/GestaoAcademicaApp1.java
@@ -17,6 +17,7 @@ public class GestaoAcademicaApp1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+        System.out.println("Teste Git Netbeans");
     }
     
     // 3 - consulta de informações de estudantes por disciplina
